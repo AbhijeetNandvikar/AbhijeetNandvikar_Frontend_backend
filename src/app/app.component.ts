2dd2f2c1bf17bf84fe81592d8831e21e424e137e
AbhijeetNandvikar <47082922+AbhijeetNandvikar@users.noreply.github.com>
2020-07-04 03:54:48 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { GetapiService } from "./getapi.service";
+import { Chart } from 'chart.js';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ball-bounce';
+  constructor(private api: GetapiService) { }
+
+  h = 0;
+  e = 0;
+  bounce = 0
+
+  onKeyH(value: string) { // without type info
+    this.h = parseInt(value, 10);
+    console.log(this.h)
+  }
+  onKeyE(value: string) { // without type info
+    this.e = parseInt(value, 10);
+    console.log(this.e)
+  }
+
+  onClickButton(h, e) {
+    this.api.getCordinates(h, e)
+      .subscribe(data => {
+        this.heightArray = data[0]
+        this.timeArray = data[1]
+        this.bounce = data[2]
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: this.timeArray,
+            datasets: [
+              {
+                data: this.heightArray,
+                borderColor: "#3cba9f",
+                fill: false
+              }
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            }
+          }
+        });
+
+      })
+  }
+  heightArray = []
+  timeArray = []
+  chart: any = []
+  ngOnInit() {
+
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+
+import { GetapiService } from "./getapi.service";
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [GetapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
